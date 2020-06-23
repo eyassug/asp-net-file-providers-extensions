a9072c283a472a7d477418603bef498a3260dc59
Eyassu Getachew <eyassug@gmail.com>
2020-06-23 19:32:51 +0300

    added GoogleCloudStorage IFileProvider implementation and test

---
+++ src/Extensions.FileProviders.Google/GoogleCloudStorageFileProvider.cs
@@ -0,0 +1,67 @@
+﻿namespace Extensions.FileProviders.Google
+{
+    using System;
+    using System.IO;
+    using Microsoft.Extensions.FileProviders;
+    using global::Google.Apis.Auth.OAuth2;
+    using global::Google.Cloud.Storage.V1;
+    using Microsoft.Extensions.Primitives;
+    using System.Threading.Tasks;
+
+    public class GoogleCloudStorageFileProvider : IFileProvider, IStorageClient
+    {
+        #region Fields
+        readonly StorageClient _storageClient;
+        readonly string _bucketName;
+        #endregion
+
+        #region Constructors
+        public GoogleCloudStorageFileProvider(GoogleCredential googleCredential, string bucketName) : this(StorageClient.Create(googleCredential), bucketName)
+        {
+            
+        }
+
+        public GoogleCloudStorageFileProvider(string googleCredentialFilePath, string bucketName) : this(GoogleCredential.FromFile(googleCredentialFilePath), bucketName)
+        {
+
+        }
+
+        public GoogleCloudStorageFileProvider(StorageClient storageClient, string bucketName)
+        {
+            _bucketName = string.IsNullOrEmpty(bucketName) ? throw new ArgumentNullException(nameof(bucketName)) : bucketName;
+            _storageClient = storageClient ?? throw new ArgumentNullException(nameof(storageClient));
+            _bucketName = bucketName;
+        }
+
+        #endregion
+
+        #region IFileProvider implementation
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region IStorageClient implementation
+        public Task<string> UploadAsync(Stream fileStream, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ test/Extensions.FileProviders.Google.Tests/GoogleCloudStorageFileProviderTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Extensions.FileProviders.Google.Tests
+{
+    using System;
+    using Xunit;
+    using global::Google.Apis.Auth.OAuth2;
+    using global::Google.Cloud.Storage.V1;
+
+    public class GoogleCloudStorageFileProviderTests
+    {
+        //readonly GoogleCloudStorageFileProvider _fileProvider;
+        
+        [Fact]
+        public void NullStorageClient_ThrowsArgumentNullException()
+        {
+            StorageClient storageClient = null;
+            Assert.Throws<ArgumentNullException>(() => new GoogleCloudStorageFileProvider(storageClient, "bucket"));
+        }
+
+        [Fact]
+        public void NullOrEmptyBucketName_ThrowsArgumentNullException()
+        {
+            var credential = GoogleCredential.FromAccessToken("test_token");
+            var client = StorageClient.Create(credential);
+            Assert.Throws<ArgumentNullException>(() => new GoogleCloudStorageFileProvider(client, null));
+            Assert.Throws<ArgumentNullException>(() => new GoogleCloudStorageFileProvider(client, string.Empty));
+        }
+    }
+}
