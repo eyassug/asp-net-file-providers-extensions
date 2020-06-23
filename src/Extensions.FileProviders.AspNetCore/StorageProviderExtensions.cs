a8f30286040133d686e91739d91c3abe2d91ee4b
Eyassu Getachew <eyassug@gmail.com>
2020-06-23 19:29:30 +0300

    added IFormFile Upload extension and test for IStorageClient

---
+++ src/Extensions.FileProviders.AspNetCore/StorageProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Extensions.FileProviders.AspNetCore
+{
+    using System;
+    using System.Threading.Tasks;
+    using Extensions.FileProviders;
+    using Microsoft.AspNetCore.Http;
+    public static class StorageProviderExtensions
+    {
+        public static Task<string> UploadAsync(this IStorageClient client, IFormFile file, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Extensions.FileProviders.AspNetCore.Tests/StorageClientExtensionsTests.cs
@@ -0,0 +1,9 @@
+﻿namespace Extensions.FileProviders.AspNetCore.Tests
+{
+    public class StorageClientExtensionsTests
+    {
+        public StorageClientExtensionsTests()
+        {
+        }
+    }
+}
