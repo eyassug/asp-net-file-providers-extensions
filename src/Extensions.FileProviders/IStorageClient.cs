d6b9d60077c6a45099371d69168f0bb1601c7682
Eyassu Getachew <eyassug@gmail.com>
2020-06-23 19:27:40 +0300

    added IStorageClient interface for uploading and deleting objects

---
+++ src/Extensions.FileProviders/IStorageClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensions.FileProviders
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public interface IStorageClient
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName);
+        Task DeleteAsync(string fileName);
+
+    }
+}
